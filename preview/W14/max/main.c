32f8714c559f0317e8e39bea89f016277de95d54
snnbyyds <snnbyyds@gmail.com>
2024-08-16 15:54:19 +0800

    preview: Sync 2024-08-16
    Change-Id: Ia156ffc200ce06069e3c5fce86ef96bf74ee1ff4

---
+++ preview/W14/max/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "max.h"
+
+int main(void){
+    printf("%d\n", max(114, 514));
+    return 0;
+}
---
+++ preview/W14/max/max.h
@@ -0,0 +1,4 @@
+#ifndef _MAX_H_
+#define _MAX_H_
+int max(int a, int b);
+#endif
